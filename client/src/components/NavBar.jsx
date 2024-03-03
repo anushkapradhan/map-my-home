29ec922d3b385b4768c5ec361e795d9f3bbc6553
dididip <dishapoojary67@gmail.com>
2024-03-03 09:12:10 +0530

    Blog section updated

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -128,7 +128,7 @@ export default function NavBar() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="/"
+                            href="/Profile"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Your Profile
@@ -138,7 +138,7 @@ export default function NavBar() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="/"
+                            href="/Settings"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Settings
