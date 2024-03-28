427edb8cdc6305fb921524954fca8321ed9882d7
dididip <dishapoojary67@gmail.com>
2024-03-28 13:40:36 +0530

    Tools Section Updated

---
+++ client/src/components/ToolsComponents/Overview.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Overview() {
+  return (
+    <div>
+        <center>
+            <p className='text-blue3 text-lg text-pretty font-bold mt-5 p-2'>OVERVIEW</p>
+            <p className='text-black text-3xl pt-5'>Welcome to Our Real Estate Tools</p>
+            <p className='text-gray-700 text-base p-1'>At MapMyHome, we're dedicated to providing you with the tools and resources you need to navigate <br />
+  the complex world of real estate with confidence and ease. Whether you're a first-time homebuyer, a seasoned investor, <br />
+  or a seller looking to maximize your property's value, our comprehensive suite <br />
+  of real estate tools is here to help you every step of the way.</p>
+            <div className='pl-6'>
+                <p className='text-black text-3xl pt-5'>Get Started Today:</p>
+                <p className='text-gray-700 text-base p-2'>
+                Embark on your real estate journey with confidence and ease by exploring our comprehensive suite of real estate<br/> tools. Whether you're a prospective homebuyer, an investor seeking profitable opportunities, or a seller looking to maximize returns, <br/>our platform offers a range of features designed to meet your diverse needs. Take advantage of our intuitive tools menu <br/>to discover a wealth of resources at your fingertips, from mortgage calculators to property <br/>search tools and market insights.
+                <br/><br/>Customize your experience by inputting your specific criteria and preferences, allowing you to tailor our tools to match<br/> your unique requirements. As you explore, gain valuable insights into market trends, property values, and financial projections, <br/>empowering you to make informed decisions at every step of your real estate journey.
+                <br/><br/>Sign up for an account to unlock additional benefits, including the ability to save your searches, receive personalized <br/>recommendations, and stay informed about the latest developments in the real estate market. Our dedicated support team is also <br/>here to assist you along the way, providing personalized assistance, <br/>troubleshooting, and feedback to ensure your experience with our real estate tools is seamless and rewarding.
+                <br/><br/>Ready to take control of your real estate journey? Start exploring our tools now and discover the power of informed <br/>decision-making in the world of real estate.</p>
+            </div>
+        </center>
+    </div>
+  )
+}
