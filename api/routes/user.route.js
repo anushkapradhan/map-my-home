668bad8863d8522ba90fd966639fbe30a8977af4
anushkapradhan <anushkapradhan18@gmail.com>
2024-03-30 03:18:44 +0530

    over to you neha

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,6 +8,5 @@ router.get('/test', test);
 router.post('/update/:id', verifyToken ,updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
 router.get('/listings/:id', verifyToken, getUserListings);
-//router.get('/:id', verifyToken, getUser);
 
 export default router;
